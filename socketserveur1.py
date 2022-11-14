4e3ac9eea9469299bda5ea0f5b835da0323f605d
Sarah <94045273+Sarahgrnt@users.noreply.github.com>
2022-11-14 11:41:04 +0100

    exerice 1

--- socketserveur1.py
+++ socketserveur1.py
@@ -6,7 +6,11 @@ server_socket.bind(("127.0.0.1", 10000))
 print('serveur démaré')
 server_socket.listen(1)
 conn, address = server_socket.accept()
-conn.send(input().encode())
-data = conn.recv(1024).decode()
-print(data)
+msg=""
+while msg !="bye":
+    msg = conn.recv(1024).decode()
+    print(msg)
+    test= input("message:")
+    conn.send(test.encode())
+
 conn.close()
