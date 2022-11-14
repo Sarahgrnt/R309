4f196c12212b352480c7f57a35b5cb80929ada3e
Sarah <94045273+Sarahgrnt@users.noreply.github.com>
2022-11-14 10:33:13 +0100

    test

--- client_socket.py
+++ client_socket.py
@@ -6,9 +6,9 @@ client_socket.connect(("127.0.0.1", 10000))
 print ("connecté au serveur")
 client_socket.send(input().encode())
 data = client_socket.recv(1024).decode()
-if data == "stop":
-    client_socket.close()
-else:
-    client_socket.send(input().encode())
-print(data)
+#if data == "bye":
+    #client_socket.close()
+#else:
+    #client_socket.send(input().encode())
+#print(data)
 client_socket.close()
