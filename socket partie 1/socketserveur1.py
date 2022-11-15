a0d12deb235f87ddc657f2ca44430423515dd6f4
Sarah <94045273+Sarahgrnt@users.noreply.github.com>
2022-11-15 09:57:01 +0100

    ca marche

--- socket partie 1/socketserveur1.py
+++ socket partie 1/socketserveur1.py
@@ -5,15 +5,21 @@ server_socket = socket.socket()
 server_socket.bind(("127.0.0.1", 10000))
 print('serveur démaré')
 server_socket.listen(1)
-conn, address = server_socket.accept()
 msg=""
 test=""
 
 while msg !="arret" and test!="arret":
-    msg = conn.recv(1024).decode()
-    print(msg)
-    test= input("message:")
-    conn.send(test.encode())
+    conn, address = server_socket.accept()
+    msg = ""
+    test = ""
+    while msg !="bye" and test!="bye" and msg !="arret" and test!="arret":
+        msg = conn.recv(1024).decode()
+        print(msg)
+        if msg =="bye":
+            conn.send("bye".encode())
+        else:
+            test= input("message:")
+            conn.send(test.encode())
 
 
 
